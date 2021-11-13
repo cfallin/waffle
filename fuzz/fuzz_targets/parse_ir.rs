16a645e69b3c4da3db4d581d2a619877bc8bc82b
Chris Fallin <chris@cfallin.org>
2021-11-13 14:31:11 -0800

    Add fuzz target to fuzz wasm-to-IR conversion.

---
+++ fuzz/fuzz_targets/parse_ir.rs
@@ -0,0 +1,9 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+
+use waffle::frontend::wasm_to_ir;
+use wasm_smith::Module;
+
+fuzz_target!(|module: Module| {
+    let _parsed_module = wasm_to_ir(module.to_bytes()).unwrap();
+});
